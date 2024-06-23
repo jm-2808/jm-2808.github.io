5005ff84bf14c89d4d19b9012207b03ba6702091
Jack Magnay <jack.magnay.2808@gmail.com>
2024-06-23 02:19:13 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+//=== General =========================================================================================
+
+var currentTheme = "light";
+var currentPage = "Home";
+
+// Toggle between light and dark themes
+function ToggleTheme()
+{
+    SetTheme((currentTheme == "light" ? "dark" : "light"));
+}
+
+// Show the specified page and hide any other pages
+function ShowPage(id)
+{
+    if(id == currentPage)
+        return;
+
+    if(currentPage == "Home")
+    {
+        SetPageVisibility(id, true);
+    }
+    else
+    {
+        SetPageVisibility(currentPage, false);
+        setTimeout(function() { SetPageVisibility(id, true); }, 250);
+    }
+
+    SetTitle(id);
+    currentPage = id;
+}
+
+//--- Helper Functions ----------------------------------------
+
+// Set the title of the webpage 
+function SetTitle(text)
+{
+    document.title = "Portfolio | " + text.toUpperCase();
+}
+
+// Set the visibility of a page (make sure delays match the transition duration of the page class in css file)
+function SetPageVisibility(id, visible)
+{
+    const element = document.getElementById(id);
+
+    if(visible)
+    {
+        element.style.width = "70%";
+        setTimeout(function() { element.style.maxHeight = element.scrollHeight + "px"; }, 250);
+    }
+    else
+    {
+        element.style.maxHeight = "0";
+        setTimeout(function() { element.style.width = 0; }, 250);
+    }
+}
+
+// Set the website colour scheme
+function SetTheme(theme)
+{
+    currentTheme = theme;
+    const root = document.querySelector(':root');
+    root.style.setProperty("--primary", "var(--" + theme + "-primary)");
+    root.style.setProperty("--secondary", "var(--" + theme + "-secondary)");
+    root.style.setProperty("--tertiary", "var(--" + theme + "-tertiary)");
+    root.style.setProperty("--text", "var(--" + theme + "-text)");
+    root.style.setProperty("--border", "var(--" + theme + "-border)");
+}
+
+
+//=== Easter Eggs =====================================================================================
